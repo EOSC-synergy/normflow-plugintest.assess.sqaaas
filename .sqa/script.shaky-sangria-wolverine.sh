ad0d4bc8ea752e321b57f28f350795ddb2923e26
samuelbernardolip <44866834+samuelbernardolip@users.noreply.github.com>
2025-11-03 12:05:43 +0000

    Add JePL file structure

--- .sqa/script.shaky-sangria-wolverine.sh
+++ .sqa/script.shaky-sangria-wolverine.sh
@@ -5,5 +5,5 @@
 
 (
 cd github.com/PalomoIFCA/normflow-plugintest &&
-    python3 -m venv .venv .venv/bin/pip install . .venv/bin/pytest -v tests/
+    python3 -m venv .venv ;.venv/bin/pip install .; .venv/bin/pytest 
 )
