e5fc88a767e01ef0b75fa236835387e82086674e
PalomoIFCA <143794185+PalomoIFCA@users.noreply.github.com>
2025-11-03 09:11:13 +0100

    Add JePL file structure

---
+++ .sqa/script.paltry-gold-ragdoll.sh
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd github.com/PalomoIFCA/normflow-plugintest &&
+    python -m venv .venv &&&&
+    source .venv/bin/activate &&&&
+    .venv/bin/pip install &&&&
+    .venv/bin/pytest -v tests/
+)
